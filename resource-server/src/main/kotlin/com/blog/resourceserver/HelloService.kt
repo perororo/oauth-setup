1972b62fb476e12b5c0680be0db9e34219bc8d89
prole <p.prole@reply.de>
2024-12-26 14:54:11 +0100

    enable method security

--- resource-server/src/main/kotlin/com/blog/resourceserver/HelloService.kt
+++ resource-server/src/main/kotlin/com/blog/resourceserver/HelloService.kt
@@ -2,13 +2,15 @@ package com.blog.resourceserver
 
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.stereotype.Service
 
 @Service
 class HelloService {
     @PreAuthorize("hasAuthority('SCOPE_user.read')")
     fun hello(): String {
-        val someUserInfo = SecurityContextHolder.getContext().authentication.principal
-        return "Hello: ${someUserInfo}!"
+        val jwt = SecurityContextHolder.getContext().authentication.principal as Jwt
+        val userSubject = jwt.subject
+        return "Hello: ${userSubject}!"
     }
 }
--- resource-server/src/main/kotlin/com/blog/resourceserver/ResourceServerApplication.kt
+++ resource-server/src/main/kotlin/com/blog/resourceserver/ResourceServerApplication.kt
@@ -2,8 +2,10 @@ package com.blog.resourceserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 
 @SpringBootApplication
+@EnableMethodSecurity
 class ResourceServerApplication
 
 fun main(args: Array<String>) {
